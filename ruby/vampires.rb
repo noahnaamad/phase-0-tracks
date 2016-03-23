a6484c81fb28c2e0b87f686c867323a7320f41db
noahnaamad <noahnaamad@gmail.com>
2016-03-22 19:25:48 -0700

    Add questions and assign them to variables

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+print "What is your name?"
+name = gets.chomp
+name.capitalize!
+print "How old are you?"
+age1 = gets.chomp
+print "What year were you born?"
+year = get.chomp
+age2 = 2016 - year
+age3 = 2015 - year
+print "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+print "Would you like to enroll in the company’s health insurance?"
+insurance = get.chomp
