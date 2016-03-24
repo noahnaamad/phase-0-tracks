714026b43981654ffd062708d24e27a513b49269
noahnaamad <noahnaamad@gmail.com>
2016-03-23 18:45:30 -0700

    Convert input to appropriate datatypes

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,11 +3,15 @@ name = gets.chomp
 name.capitalize!
 print "How old are you?"
 age1 = gets.chomp
+age1 = age.to_i
 print "What year were you born?"
-year = get.chomp
+year = gets.chomp
+year = year.to_i
 age2 = 2016 - year
 age3 = 2015 - year
-print "Our company cafeteria serves garlic bread. Should we order some for you?"
+print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic = gets.chomp
-print "Would you like to enroll in the company’s health insurance?"
-insurance = get.chomp
+garlic.downcase!
+print "Would you like to enroll in the company’s health insurance? (y/n)"
+insurance = gets.chomp
+insurance.downcase!
