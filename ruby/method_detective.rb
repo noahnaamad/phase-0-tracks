cc395d768e90a19f76e2c3c10087a284c12580cc
noahnaamad <noahnaamad@gmail.com>
2016-03-28 16:41:11 -0700

    Find more solutions

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,6 +4,8 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
+##TO DO: DISAPPEARING FIRST AND LAST LETTERS
+
 "iNvEsTiGaTiOn".swapcase
 "iNvEsTiGaTiOn".replace "InVeStIgAtIoN"
 # => "InVeStIgAtIoN"
