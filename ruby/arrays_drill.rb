bfe08258e18acceecf0e992ba1d35e09123e2689
noahnaamad <noahnaamad@gmail.com>
2016-03-28 17:43:17 -0700

    Add code from driver

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,9 @@
+def buildArray(a,b,c)
+ e = []
+ e << a 
+ e << b
+ e << c
+end
 x = []
 p x
 x << 1
@@ -6,3 +12,20 @@ x << 3
 x << 4
 x << 5
 p x
+x.delete_at(2)
+p x 
+x.insert(2,3.0)
+p x 
+x.delete(1)
+p x
+y = x.include?(3.0) 
+if y == true
+ puts "it includes this object."
+else 
+ puts "it does not include this object."
+end
+z = ["blue", "red", "grey", "maroon"]
+aa = z + x
+p aa
+
+buildArray(1,2,3)
