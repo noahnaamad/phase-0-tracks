6dca18213dc4b2ac40daafede92ac72fde3a0df7
noahnaamad <noahnaamad@gmail.com>
2016-03-31 21:40:21 -0700

    Produce pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+#create empty hash
+#ask user for information, convert to correct type
+#then store it in the "value" field in hash with relevant key i create
+
+
+#print the hash
+
+
+#prompt user to tell me if they want to change a thing
+#if they change it, convert the string into a symbol
+#correct the symbol's value
+
+#print latest version of hash
