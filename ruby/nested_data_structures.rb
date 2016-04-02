633bebb42066ae545ec2b94a8d553d2ca31ed7e3
noahnaamad <noahnaamad@gmail.com>
2016-04-02 15:13:30 -0700

    Build nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,33 @@
+#i'm going to make a gang of cat friends
+#each cat is going to have a name, an age, a list of physical characteristics, and a list of favorite toys
+
+cat_friends = {
+	cat1: {
+		name: "Spanky",
+		age: 12,
+		looks: ["medium size", "tuxedo fur", "funny mustache", "long whiskers"],
+		toys: ["human faces", "headphone cords", "cardboard boxes", "grass"]
+	}
+
+	cat2: {
+		name: "Treasa",
+		age: 8,
+		looks: ["medium size", "black fur", "long-haired", "medium whiskers"],
+		toys: ["mice", "stuffed animals", "cardboard boxes", "string"]
+	}
+
+	cat3: {
+		name: "Noah",
+		age: 3,
+		looks: ["small size", "calico fur", "extra fluffy" "medium whiskers"],
+		toys: ["necklaces", "headphone cords", "cardboard boxes", "bugs in the house"]
+	}
+
+	cat4: {
+		name: "Anna",
+		age: 1,
+		looks: ["long and slender", "tabby", "long whiskers", "extra soft hair"],
+		toys: ["game console cords", "headphone cords", "cardboard boxes", "pens"]
+	}
+
+}
