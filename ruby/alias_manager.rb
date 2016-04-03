6eed5bb80d822ffe4fd621fc68ed6c4f2c6eb8af
noahnaamad <noahnaamad@gmail.com>
2016-04-02 17:54:35 -0700

    Create pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,12 @@
+def to_alias(name)#take string
+	new_alias = name.split(' ')
+	puts new_alias
+	#alias.reverse! #now i have reversed the first and last name and put the name in an array
+#swap first and last name
+#loop through the string
+#if vowel, call method to determine next vowel
+#change that vowel to next vowel (exclude y) and concatenate to alias string
+#elsif consonant, call metho to determine next consonant
+#change consonant to next consonant
+#else keep it (in case it's a space or dash or whatever)
+end
