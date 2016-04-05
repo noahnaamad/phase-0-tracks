d890c94ca36c3b0ccb28f8e0138420c0e30e3243
noahnaamad <noahnaamad@gmail.com>
2016-04-04 19:32:40 -0700

    Add attributes and driver code to test attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,14 @@
 class Santa
 
+	def initialize (gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+		puts "Initializing Santa instance...."
+	end
+
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -8,15 +17,22 @@ class Santa
 		puts "That was a good #{cookie}"
 	end
 
-	def initialize
-		puts "Initializing Santa instance...."
-	end
-
 end
 
 #driver code
+=begin
 Sant = Santa.new
 
 Sant.speak
 
 Sant.eat_milk_and_cookies("thin mint")
+=end
+
+#initializations driver code
+santas = []
+santas << Santa.new("genderqueer", "jewish mixed-race")
+santas << Santa.new("woman", "white")
+santas << Santa.new("genderqueer demigirl femme", "white")
+santas << Santa.new("questioning", "black")
+
+p santas
