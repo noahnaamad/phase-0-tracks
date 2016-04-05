c6d0418e753ec305bedd3608e4bad0afe7f4a790
noahnaamad <noahnaamad@gmail.com>
2016-04-04 18:04:55 -0700

    Pseudocode characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+=begin
+SPECIES --------------
+Tineus Doggius
+
+CHARACTERISTICS ------
+Color: varies
+Skin covering: fur
+Fur length: varies
+Friendly: extremely
+Eye count: 2
+Leg count: 4
+Name: varies
+Size: varies
+
+BEHAVIOR ------------
+Lick
+Chase tail
+Poop on the sidewalk
+Snuggle
+Eat
+Bark endlessly
+Fetch
+
+=end
