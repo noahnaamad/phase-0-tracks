c3cd13589a95115be3206067c43c983462c666e7
noahnaamad <noahnaamad@gmail.com>
2016-04-06 14:42:45 -0700

    Method to delete, method to access certain item

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -8,10 +8,15 @@ class TodoList
 		@things_to_do
 	end
 
+	def get_item(chore_index)
+		@things_to_do[chore_index]
+	end
+
 	def add_item(new_chore)
 		@things_to_do << new_chore
 	end
 
-	def delete_item
+	def delete_item(chore_done)
+		@things_to_do.delete(chore_done)
 	end
 end
