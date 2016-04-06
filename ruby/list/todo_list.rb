5e501461caad5e87e00942084edee37a594e639d
noahnaamad <noahnaamad@gmail.com>
2016-04-06 14:32:55 -0700

    Add function

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,6 +1,17 @@
 class TodoList
-	def initialize(dishes, mow)
-		@dishes = dishes
-		@mow = mow
+	
+	def initialize(things_to_do)
+		@things_to_do = things_to_do
+	end
+
+	def get_items
+		@things_to_do
+	end
+
+	def add_item(new_chore)
+		@things_to_do << new_chore
+	end
+
+	def delete_item
 	end
 end
