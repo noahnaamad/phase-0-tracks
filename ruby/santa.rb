908d85cfb344534f5ff6bbce2cef37d770cdee80
noahnaamad <noahnaamad@gmail.com>
2016-04-07 21:22:18 -0700

    Add celebrate birthday and get mad at methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,15 @@ class Santa
 		puts "That was a good #{cookie}"
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at (reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+	end
+
 end
 
 #driver code
@@ -28,6 +37,7 @@ Sant.speak
 Sant.eat_milk_and_cookies("thin mint")
 =end
 
+=begin
 #initializations driver code
 santas = []
 santas << Santa.new("genderqueer", "jewish mixed-race")
@@ -36,3 +46,12 @@ santas << Santa.new("genderqueer demigirl femme", "white")
 santas << Santa.new("questioning", "black")
 
 p santas
+=end
+
+#birthday and get mad at driver code
+nick = Santa.new("gq", "native american")
+p nick
+nick.celebrate_birthday
+p nick
+nick.get_mad_at("Dasher")
+p nick
