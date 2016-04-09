b1135284f97a450402d0fcc8bf40c54ae71c93d0
noahnaamad <noahnaamad@gmail.com>
2016-04-08 22:57:00 -0700

    Add driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -8,3 +8,7 @@ module Shout
   		"I'M SO HAPPY BECAUSE " + words + "!!!"
   	end
 end
+
+#driver code
+p Shout.yell_angrily("the patriarchy sucks")
+p Shout.yelling_happily("my partner is the best ever")
