38c2a602cd238aef97b22a57c1d3949653e9aef2
noahnaamad <noahnaamad@gmail.com>
2016-04-08 20:12:03 -0700

    Pseudocode for TV shows class

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+=begin
+This class creates TV shows
+Attributes: name (string), genre (string), number of seasons so far(int), number of episodes aired this season(int), rating out of 5 (float), main characters(array)	
+
+Methods:
+-Create a new season
+-Air the show
+-Fridge the girlfriend to move the plot along (takes some woman or minority character)
+=end
