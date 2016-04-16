740f29e792c632681535a4aff0ee9b66d75bf8a0
noahnaamad <noahnaamad@gmail.com>
2016-04-15 22:28:25 -0700

    Pseudocode and create function to decide whether two objects share key/value pair, create driver code for both true and false

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+//this function returns the length of the longest phrase in an array
 function lengthOfLongest(array, arrayLength){
 	var lengthOfLongestPhrase = 0;
 	for (i=0; i<arrayLength; i++){
@@ -10,6 +11,7 @@ function lengthOfLongest(array, arrayLength){
 	return lengthOfLongestPhrase;
 };
 
+//this function returns the actual longest phrase(s) in an array of phrases
 function longPhrase(array){
 	var arrayLength = array.length;
 	var lengthiest = lengthOfLongest(array, arrayLength);
@@ -29,12 +31,41 @@ function longPhrase(array){
 
 }
 
-//write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+//write a function that takes two objects and checks to see if the objects share at least one key-value pair. 
+//PSEUDOCODE:
+//0) set a variable to false
+//1) iterate through first object
+//2) for each key/value pair in first object, iterate through second object and check if they are equal
+//3) if they are, change the variable from (0) to true
+//4) return variable from (0)
+
+function share(obj1, obj2){
+	var theyshare = false;
+	for (var key1 in obj1){
+		if (obj1.hasOwnProperty(key1)){
+			for (var key2 in obj2){
+				if (obj2.hasOwnProperty(key2)){
+					if ((key1 == key2) && (obj1[key1] == obj2[key2])){
+						theyshare = true;
+						//console.log(obj[key1]);
+					};
+				};
+			};
+		};
+	};
+
+	return theyshare;
+};
+
+//driver code share
+console.log(share({name: "Steven", age: 54, gender: "agender"}, {name: "Tamir", age: 54, gender: "genderqueer", height: 68}));
+console.log(share({name: "Steven", age: 30, gender:"bigender"}, {name: "Tamir", age: 98, height: 67}));
 
 
 //driver code longPhrase
 // console.log(longPhrase(["1234", "12345678", "12345", "1"]));
 // console.log(longPhrase(["long phrase","longest phrase","longer phrase"]));
 // console.log(longPhrase(["yes", "now", "dog", "cat", "bee", "ah"]));
+
 //driver code lengthOfLongest
 //console.log(lengthOfLongest(["123456", "12345678", "1234567890"]));
