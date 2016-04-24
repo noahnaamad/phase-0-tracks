3e8a55199ca5a0f24c28392505af5214c8d8628e
noahnaamad <noahnaamad@gmail.com>
2016-04-23 21:04:00 -0700

    Attempt to extract driver names and times

--- databases/notmariokart.rb
+++ databases/notmariokart.rb
@@ -38,33 +38,65 @@ if exists == [[0]]
 	end
 end
 
+#make the drivers more acccessible
+the_drivers = db.execute("SELECT * FROM drivers")
 
 #calculate the time it took the driver to complete this part of the course
+#they guess the roll of a die -- if their guess faces up they go fast, if their guess faces down they go slow, otherwise they go medium.
+
 def time_driving(guess)
 	time_spent = 0
 	right_guess = rand(6) + 1
 	if guess == right_guess
 		time_spent = 1
 	elsif guess == 7 - right_guess
-		time_spent = 3
+		time_spent = 5
 	else
-		time_spent = 2
+		time_spent = 3
 	end
+	puts "right guess was #{right_guess} and time spent is #{time_spent}"
 end
 
 #add the time it took to complete that part of the course to the total time spent driving
+
 def time_update(db, driver_id, time_spent)
-	add_time = <<-SQL
-		UPDATE drivers SET curr_time = curr_time + time_spent WHERE driver.id = driver_id
-	SQL
+	drivers = db.execute("SELECT * FROM drivers")
+	add_time = "UPDATE drivers SET curr_time = curr_time + #{time_spent} WHERE drivers.id = #{driver_id}"
 	db.execute(add_time)
+	
 end
 
+#create hash of drivers and their total time
+driver_times = <<-SQL
+	SELECT drivers.name, drivers.curr_time FROM drivers
+SQL
+x = db.execute(driver_times)
+puts x
 
 =begin
+#driver code create and populate table - but only once!!
 the_drivers = db.execute("SELECT * FROM drivers")
 puts "exists is #{exists}"
 puts the_drivers
+
+
+#driver code time_driving
+time_driving(5)
+time_driving(2)
+time_driving(5)
+time_driving(2)
+time_driving(5)
+time_driving(2)
+time_driving(5)
+time_driving(2)
+
+#driver code time_update
+time_update(db, 1, 5)
+time_update(db, 1, 5)
+time_update(db, 3, 1)
+time_update(db, 5, 3)
+puts the_drivers
+
 
 20.times do 
 	create_kitten(db, Faker::Name.name, 0)
