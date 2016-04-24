32b77ed2491992c449fe04adf62ab376b2e5f779
noahnaamad <noahnaamad@gmail.com>
2016-04-23 17:13:42 -0700

    driver code to test if database already exists

--- databases/notmariokart.rb
+++ databases/notmariokart.rb
@@ -19,15 +19,25 @@ db.execute(create_table_cmd)
 already_existed = <<-SQL
 	SELECT COUNT(*) FROM drivers;
 SQL
+
+puts already_existed
 #if already_existed = 0  i will populate the DB with the characters, otherwise it is already populated and I shouldn't add the same characterss again!!
 
 #db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
-def create_driver(db, name, time, points)
-	db.execute("INSERT INTO drivers (name, curr_time, points) VALUES (?, ?)", [name, time, points])
+=begin
+characters = ["bowser", "toad", "peach", "yoshi", "mario", "wario", "luigi", "you"]
+
+if already_existed == 0
+
 end
 
+def create_driver(name)
+	db.execute("INSERT INTO drivers (name, 0, 0) VALUES (?)", [name])
+end
+=end
 
+=begin
 20.times do 
 	create_kitten(db, Faker::Name.name, 0)
 end
@@ -42,3 +52,4 @@ kittens.each do |kit|
 end 
 
 puts kittens
+=end
