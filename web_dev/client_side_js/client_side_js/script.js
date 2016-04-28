e21834394e94b36cd6f16f4d7ec8bf9aef5a92be
noahnaamad <noahnaamad@gmail.com>
2016-04-28 00:55:04 -0700

    Update directory

--- web_dev/client_side_js/client_side_js/script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello!");
-
-var thePic = document.getElementById("pointerPic");
-var secondPic = thePic.cloneNode(false);
-var theDiv = document.getElementById("theDiv");
-theDiv.appendChild(secondPic);
